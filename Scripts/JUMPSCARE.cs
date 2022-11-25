e12568adfd88443ddd7ebf27ee638058646216b9
Kenza5555 <89758918+Kenza5555@users.noreply.github.com>
2022-11-25 22:09:19 +0700

    Add files via upload

---
+++ Scripts/JUMPSCARE.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JUMPSCARE : MonoBehaviour
+{
+    public GameObject JumpScareUI;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            JumpScareUI.SetActive(true);
+            Invoke("hide", 1);
+        }
+    }
+    public void hide()
+    {
+        JumpScareUI.SetActive(false);
+    }
+    private void Start()
+    {
+        hide();
+    }
+}
